f700893197734e58a621e484ffc3dd9cb597645f
Alielsalek1 <ali4business314@gmail.com>
2023-12-25 14:07:11 +0200

    adjusted the .cpp file

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -5,23 +5,19 @@ void Queue<T>::enqueue(T val) {
     data.push_back(val);
     num_elements++;
 }
-
 template <typename T>
 void Queue<T>::dequeue() {
     data.pop_front();
     num_elements--;
 }
-
 template <typename T>
 T Queue<T>::front() {
     return data.front();
 }
-
 template <typename T>
 bool Queue<T>::empty() {
     return !num_elements;
 }
-
 template <typename T>
 int Queue<T>::size() {
     return num_elements;
